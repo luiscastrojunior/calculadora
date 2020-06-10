7f53ce8270bcf424d3bac571307d09c6b5ddf097
luiscastro2020 <64877755+luiscastro2020@users.noreply.github.com>
2020-06-10 19:14:12 -0300

    atualização do projeto calculadora
    atualização da função pow e menus

--- src/Calculadora.cpp
+++ src/Calculadora.cpp
@@ -1,12 +1,11 @@
-//============================================================================
-// Name        : Calculadora.cpp
-// Author      : Luis Castro
-// Version     :
-// Copyright   : 
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
+/*
+ * teste.cpp
+ *
+ *  Created on: 9 de jun de 2020
+ *      Author: Jr_Castro
+ */
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 double soma (double numa, double numb) {
@@ -26,47 +25,39 @@ double divisao (double numa, double numb) {
 }
 
 double exponenciacao (double numa, double numb) {
-	double numc, result;
-	numc = 1;
-	if (numb == 0){
-		return numc;
-	}
-	else {
-		result = numa;
-		while (numc < numb) {
-			result = result * numa;
-			numc = numc + 1;
-			}
-		return result;}
+	return pow(numa, numb);
 }
 
 int main() {
 	double num1, num2;
-	int operacao;
-	cout << "Calculadora" << endl;
-	cout << "Digite o 1º número: "; cin >> num1;
-	cout << "1 - (+) Adição" << endl;
-	cout << "2 - (-) Subtração" << endl;
-	cout << "3 - (*) Multiplicação" << endl;
-	cout << "4 - (/) Divisão" << endl;
-	cout << "5 - (^) Exponenciação" << endl;
-	cout << "Digite a operação matemática que deseja fazer: ";
-	cin >> operacao;
-	cout << "Digite o 2º número: ";
-	cin >> num2;
-	switch (operacao) {
-			case 1: {cout << num1; cout << " + "; cout << num2; cout << " = ";  cout << soma(num1, num2);}
-				break;
-			case 2: {cout << num1; cout << " - "; cout << num2; cout << " = ";  cout << subtracao (num1, num2);}
-				break;
-			case 3: {cout << num1; cout << " * "; cout << num2; cout << " = ";  cout << multiplicacao (num1, num2);}
-				break;
-			case 4:
-				if (num2 != 0) {cout << num1; cout << " / "; cout << num2; cout << " = ";  cout << divisao (num1, num2);}
-				else cout << "Operação Inválida";
-				break;
-			case 5: {cout << num1; cout << " ^ "; cout << num2; cout << " = ";  cout << exponenciacao (num1, num2);}
-				break;
-			default: cout << "Operaçao inválida!!";}
-	return 0;
+	char operacao;
+	while (true) {
+		num1 = 0;
+		num2 = 0;
+		cout << "Calculadora" << endl;
+		cout << "Digite o 1º número: "; cin >> num1;
+		cout << "(+) Adição" << endl;
+		cout << "(-) Subtração" << endl;
+		cout << "(*) Multiplicação" << endl;
+		cout << "(/) Divisão" << endl;
+		cout << "(^) Exponenciação (Potência)" << endl;
+		cout << "Digite a operação matemática que deseja fazer: ";
+		cin >> operacao;
+		cout << "Digite o 2º número: ";
+		cin >> num2;
+		switch (operacao) {
+				case '+': {cout << num1; cout << " + "; cout << num2; cout << " = ";  cout << soma(num1, num2); cout << endl;}
+					break;
+				case '-': {cout << num1; cout << " - "; cout << num2; cout << " = ";  cout << subtracao (num1, num2); cout << endl;}
+					break;
+				case '*': {cout << num1; cout << " * "; cout << num2; cout << " = ";  cout << multiplicacao (num1, num2); cout << endl;}
+					break;
+				case '/':
+					if (num2 != 0) {cout << num1; cout << " / "; cout << num2; cout << " = ";  cout << divisao (num1, num2); cout << endl;}
+					else cout << "Operação Inválida";
+					break;
+				case '^': {cout << num1; cout << " ^ "; cout << num2; cout << " = ";  cout << exponenciacao (num1, num2); cout << endl;}
+					break;
+				default: cout << "Operaçao inválida!!";}
+	}
 }
